72dcb7182839af69c93a6492e417ea1da146a47d
LIN-ai <52541517+LIN-ai@users.noreply.github.com>
2019-07-11 06:21:32 +0800

    Add files via upload

---
+++ HW9.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class Solution(object):
+    def fib(self, N):
+        """
+        :type N: int
+        :rtype: int
+        """
+        F = [i for i in range(N+1)]
+        for i in range(2,N+1):
+            F[i] = F[i-1]+F[i-2]
+        return F[-1]
+        
+

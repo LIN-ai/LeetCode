2446d492c00d3727307956390b2ae944d92b70b1
LIN-ai <52541517+LIN-ai@users.noreply.github.com>
2019-07-11 04:58:12 +0800

    Add files via upload

---
+++ HW1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+class Solution(object):
+    def toLowerCase(self, str):
+        """
+        :type str: str
+        :rtype: str
+        """
+        s =str.lower()
+        
+        return s
+
+
+# In[ ]:
+
+
+
+

17e3c39e9085223807e78cf80295defe650d13a9
LIN-ai <52541517+LIN-ai@users.noreply.github.com>
2019-07-11 10:39:35 +0800

    Add files via upload

---
+++ HW10.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        i = 0
+        j = len(s) - 1
+        while (i < j):
+            s[i], s[j] = s[j], s[i]
+            i+=1
+            j-=1
+            
+            
+
